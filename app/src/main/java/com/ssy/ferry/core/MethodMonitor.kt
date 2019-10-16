1e7bf6d0584d3301e8e0da392cc29c252be438dc
ssy <shishaoyan@do-global.com>
2019-10-16 20:01:14 +0800

    change:..

--- app/src/main/java/com/ssy/ferry/core/MethodMonitor.kt
+++ app/src/main/java/com/ssy/ferry/core/MethodMonitor.kt
@@ -1,23 +1,57 @@
 package com.ssy.ferry.core
 
+import android.os.SystemClock
+
 /**
  * 2019-10-16
  * @author Mr.S
  */
-class MethodMonitor : Runnable, MonitorLifecycle {
-    override fun run() {
+class MethodMonitor : MonitorLifecycle {
+
+    private val STATUS_DEFAULT = Integer.MAX_VALUE
+    var alive = false
+    var status = STATUS_DEFAULT
+
+    private val updateTimeRunnable = Runnable {
+        try {
+            while (true) {
+
+            }
+        } catch (e: InterruptedException) {
+        }
+    }
+
+
+    fun init() {
+        LooperMonitor.register(object : LooperDispatchListener() {
+            override fun isAlive(): Boolean {
+                return alive
+            }
+
+            override fun dispatchStart() {
+                super.dispatchStart()
+
+            }
+
+            override fun dispatchEnd() {
+                super.dispatchEnd()
+            }
+        })
     }
 
     override fun start() {
+        alive = true
     }
 
     override fun stop() {
+        alive = false
     }
 
     companion object {
         open fun i(index: Int) {
 
         }
+
         open fun o(index: Int) {
 
         }
